376c77d8bb09573b03306d81273cb1c3089fe665
Jason Swetzoff <swetzoff@gmail.com>
2015-04-02 12:31:09 -0700

    First Commit - Building Sass compiler in Gulp. Adding packages to --save-dev. Adding readme. Adding gitignore.

---
+++ README.md
@@ -0,0 +1,10 @@
+# Gulp-Starter
+
+I am experimenting in building a workflow with Gulp for my new portfolio website.
+
+## Requienements
+
+- Live Reload
+- Live Generated Styleguide
+- Sass
+- Build and Development Environments
---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    livereload = require('gulp-livereload'),
+    styleguide = require('sc5-styleguide'),
+    del = require('del');
+
+gulp.task('styles', function() {
+    return gulp.src('src/styles/main.scss')
+        .pipe(sass({ style: 'expanded' }))
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+        .pipe(gulp.dest('builds/assets/css'))
+        .pipe(rename({ suffix: '.min'}))
+        .pipe(minifycss())
+        .pipe(gulp.dest('builds/assets/css'))
+        .pipe(notify({ message: 'Styles task complete' }));
+});
