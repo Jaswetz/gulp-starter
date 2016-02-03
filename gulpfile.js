ad968027be447e92ccc05c5409d0f025ef89bd5a
Jason Swetzoff <swetzoff@gmail.com>
2016-02-03 00:11:40 -0800

    Updating gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,106 +1,94 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    minifycss = require('gulp-minify-css'),
-    jshint = require('gulp-jshint'),
-    uglify = require('gulp-uglify'),
-    imagemin = require('gulp-imagemin'),
-    rename = require('gulp-rename'),
-    concat = require('gulp-concat'),
-    notify = require('gulp-notify'),
-    cache = require('gulp-cache'),
-    browsersync = require('browser-sync'),
-    minifyHTML = require('gulp-minify-html'),
-    styleguide = require('sc5-styleguide'),
     plumber = require('gulp-plumber'),
+    sourcemaps = require('gulp-sourcemaps'),
+    notify = require('gulp-notify'),
     gutil = require('gulp-util'),
-    outputPath = 'build/development/styleguide',
+    browserSync = require('browser-sync'),
     svgstore = require('gulp-svgstore'),
     svgmin = require('gulp-svgmin'),
     inject = require('gulp-inject'),
-    sourcemaps = require('gulp-sourcemaps'),
+    rename = require('gulp-rename'),
+    autoprefixer = require('gulp-autoprefixer'),
+    concat = require('gulp-concat'),
     del = require('del');
 
-// error function for plumber
-var onError = function (err) {
-  gutil.beep();
-  console.log(err);
-  this.emit('end');
-};
+    // files
+    var tempFolderAssets = 'temp/assets';
+
+    // Sass Error Reporting
+    //https://github.com/mikaelbr/gulp-notify/issues/81
+    var reportError = function (error) {
+        var lineNumber = (error.lineNumber) ? 'LINE ' + error.lineNumber + ' -- ' : '';
+
+        notify({
+            title: 'Task Failed [' + error.plugin + ']',
+            message: lineNumber + 'See console.',
+            sound: 'Sosumi' // See: https://github.com/mikaelbr/node-notifier#all-notification-options-with-their-defaults
+        }).write(error);
+
+        gutil.beep(); // Beep 'sosumi' again
+
+        // Inspect the error object
+        //console.log(error);
+
+        // Easy error reporting
+        //console.log(error.toString());
+
+        // Pretty error reporting
+        var report = '';
+        var chalk = gutil.colors.white.bgRed;
+
+        report += chalk('TASK:') + ' [' + error.plugin + ']\n';
+        report += chalk('PROB:') + ' ' + error.message + '\n';
+        if (error.lineNumber) { report += chalk('LINE:') + ' ' + error.lineNumber + '\n'; }
+        if (error.fileName)   { report += chalk('FILE:') + ' ' + error.fileName + '\n'; }
+        console.error(report);
+
+        // Prevent the 'watch' task from stopping
+        this.emit('end');
+    }
 
 // Browser definitions for autoprefixer
 var AUTOPREFIXER_BROWSERS = [
-  'last 3 versions',
-  'ios >= 7',
-  'android >= 4.4',
-  'bb >= 10'
+  'last 3 versions'
 ];
 
-// BrowserSync proxy
-gulp.task('browser-sync', function() {
-  browsersync({
-        server: {
-            baseDir: "build/development"
-        }
-  });
-});
+// Static Server + watching scss/html files
+gulp.task('serve', ['clean', 'compile-css', 'compile-html', 'compile-javascript', 'move-images',], function() {
 
-// BrowserSync reload all Browsers
-gulp.task('browsersync-reload', function () {
-    browsersync.reload();
-});
+    browserSync.init({
+        server: "./temp"
+    });
 
-gulp.task('fileinclude', function() {
-    // content
+    gulp.watch("src/scss/**/*.scss", ['compile-css']);
+    gulp.watch("src/**/*.html", ['compile-html']);
+    gulp.watch("src/svg/**/*.svg", ['compile-html']);
+    gulp.watch("src/js/**/*.js", ['compile-javascript']);
+    gulp.watch("src/img/**/*", ['move-images']);
 });
 
 // Compile Sass
-gulp.task('styles', function() {
-    return gulp.src('app/scss/main.scss')
-        .pipe(plumber({ errorHandler: onError }))
+gulp.task('compile-css', function() {
+    return gulp.src('src/scss/main.scss')
+        .pipe(plumber({
+            errorHandler: reportError
+        }))
         .pipe(sourcemaps.init())
-        .pipe(sass({ style: 'expanded' }))
+        .pipe(sass({
+            style: 'expanded',
+        }))
         .pipe(autoprefixer(AUTOPREFIXER_BROWSERS))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('build/development/assets/css'))
-        .pipe(browsersync.reload({ stream:true }))
-
-});
-
-// Compile Sass to Production
-gulp.task('buildStyles', function() {
-    return gulp.src('app/scss/main.scss')
-        .pipe(plumber({ errorHandler: onError }))
-        .pipe(sass({ style: 'expanded' }))
-        .pipe(autoprefixer(AUTOPREFIXER_BROWSERS))
-        .pipe(gulp.dest('build/production/assets/css'))
-        .pipe(rename({ suffix: '.min'}))
-        .pipe(minifycss())
-        .pipe(gulp.dest('build/production/assets/css'))
-        .pipe(notify({ message: 'Styles task complete' }))
-});
-
-// Minify HTML
-gulp.task('miniHtml', function() {
-    var svgs = gulp.src('app/svg/*.svg')
-        .pipe(rename({prefix: 'icn-'}))
-        .pipe(svgstore({ inlineSvg: true }));
-
-    function fileContents (filePath, file) {
-        return file.contents.toString();
-    }
-
-  gulp.src('app/**/*.html')
-    .pipe(inject(svgs, { transform: fileContents }))
-    .pipe(minifyHTML())
-    .pipe(gulp.dest('build/production/'))
-    .pipe(notify({ message: 'HTML Minified' }))
+        .pipe(gulp.dest(tempFolderAssets + '/css'))
+        .pipe(browserSync.stream())
+        .on('error', reportError);
 });
 
 // Move HTML
-gulp.task('copyHtml', function() {
+gulp.task('compile-html', function() {
 
-    var svgs = gulp.src('app/svg/*.svg')
+    var svgs = gulp.src('src/svg/*.svg')
         .pipe(rename({prefix: 'icn-'}))
         .pipe(svgstore({ inlineSvg: true }));
 
@@ -108,117 +96,33 @@ gulp.task('copyHtml', function() {
         return file.contents.toString();
     }
 
-    gulp.src('app/*.html')
+    gulp.src('src/*.html')
       .pipe(inject(svgs, { transform: fileContents }))
-      .pipe(gulp.dest('build/development'))
-      .pipe(browsersync.reload({ stream:true }))
-});
-
-
-// Compile Build JavaScript
-gulp.task('buildJs', function() {
-
-    gulp.src('bower_components/modernizr/modernizr.js')
-        .pipe(gulp.dest('build/production/assets/js/vendor'))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(uglify())
-        .pipe(gulp.dest('build/production/assets/js/vendor'));
-
-    gulp.src('bower_components/jquery/dist/jquery.js')
-        .pipe(gulp.dest('build/production/assets/js/vendor'))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(uglify())
-        .pipe(gulp.dest('build/production/assets/js/vendor'));
-
-    gulp.src(['app/js/main.js'])
-        .pipe(concat('main.js'))
-        .pipe(gulp.dest('build/production/assets/js'))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(uglify())
-        .pipe(gulp.dest('build/production/assets/js'))
-        .pipe(notify({ message: 'Scripts task complete' }));
-
+      .pipe(gulp.dest('temp'))
+      .pipe(browserSync.stream());
 });
 
 // Compile Development JavaScript
-gulp.task('devJs', function() {
-
-    gulp.src('bower_components/modernizr/modernizr.js')
-        .pipe(gulp.dest('build/development/assets/js/vendor'));
-
-    gulp.src('bower_components/jquery/dist/jquery.js')
-        .pipe(gulp.dest('build/development/assets/js/vendor'));
-
-    gulp.src(['app/js/main.js'])
+gulp.task('compile-javascript', function() {
+    gulp.src(['src/js/main.js'])
         .pipe(concat('main.js'))
-        .pipe(gulp.dest('build/development/assets/js'));
+        .pipe(gulp.dest(tempFolderAssets +'/js'))
+        .pipe(browserSync.stream());
 });
 
-
-
-// Compress Images to Build folder
-gulp.task('buildImg', function() {
-  return gulp.src('app/img/**/*')
-    .pipe(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true }))
-    .pipe(gulp.dest('build/production/assets/img'))
-    .pipe(notify({ message: 'Images task complete' }))
+// Compress Images
+gulp.task('move-images', function() {
+  return gulp.src('src/img/**/*')
+    .pipe(gulp.dest(tempFolderAssets + '/img'))
+    .pipe(browserSync.stream());
 });
 
-// Compress Images to Dev folder
-gulp.task('devImg', function() {
-  return gulp.src('app/img/**/*')
-    .pipe(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true }))
-    .pipe(gulp.dest('build/development/assets/img'))
-    .pipe(notify({ message: 'Images task complete' }))
+// Clean up
+gulp.task('clean', function () {
+  return del.sync([
+    './temp/**'
+  ]);
 });
 
-// All unprocessed styles containing the KSS markup and style variables.
-// This will process the KSS markup and collects variable information.
-gulp.task('styleguide:generate', function() {
-    return gulp.src('app/scss/**/*.scss')
-        .pipe(styleguide.generate({
-            title: 'My Styleguide',
-            rootPath: outputPath,
-            server: true,
-            port: '5000',
-            overviewPath: 'README.md'
-        }))
-        .pipe(gulp.dest(outputPath));
-});
-
-//Preprocessed/compiled stylesheets.
-// This will create necessary pseudo styles and create the actual stylesheet to be used in the styleguide.
-gulp.task('styleguide:applystyles', function() {
-    return gulp.src('app/scss/main.scss')
-        .pipe(sass({
-            errLogToConsole: true
-        }))
-        .pipe(styleguide.applyStyles())
-        .pipe(gulp.dest(outputPath));
-});
-
-// Clean up development!
-gulp.task('cleanDev', function(cb) {
-    del(['build/development/assets', 'build/development/styleguide', 'build/development/*.html'], cb)
-});
-
-// Clean up production!
-gulp.task('cleanBuild', function(cb) {
-    del(['build/production/assets', 'build/production/*.html'], cb)
-});
-
-// Watch task
-gulp.task('watch', ['browser-sync', 'styleguide'], function () {
-    gulp.watch('app/scss/**/*', ['styles', 'styleguide']);
-    gulp.watch('app/js/**/*', ['browsersync-reload', 'devJs', 'copyHtml']);
-    gulp.watch('app/**/*.html', ['browsersync-reload', 'copyHtml']);
-    gulp.watch('app/svg/*.svg', ['browsersync-reload', 'copyHtml']);
-    gulp.watch('app/img/**/*', ['browsersync-reload','devImg', 'copyHtml']);
-    // gulp.watch('build/development/**/*', ['browsersync-reload', 'styleguide']);
-});
-
-
-gulp.task('styleguide', ['styleguide:generate', 'styleguide:applystyles']);
-gulp.task('serve', [ 'styleguide', 'devImg', 'devJs', 'copyHtml', 'styles', 'watch']);
-gulp.task('build',['buildStyles', 'buildImg', 'buildJs', 'miniHtml'], function() {});
-
+// Default task
+gulp.task('default', ['serve']);
